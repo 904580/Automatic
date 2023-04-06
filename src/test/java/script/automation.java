c8938ff99ce4afbb0838ffe57896dab56f753442
Anoop Saini <Anoopsaini421@gmail.com>
2023-04-06 09:21:18 +0530

    added

---
+++ src/test/java/script/automation.java
@@ -0,0 +1,39 @@
+package script;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class automation {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		ChromeDriver driver = new ChromeDriver(options);
+		driver.get("https://www.saucedemo.com");
+		driver.manage().window().maximize();
+		
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
+		username.sendKeys("problem_user");
+		
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+		password.sendKeys("secret_sauce");
+		
+		WebElement login = driver.findElement(By.xpath("//input[@id='login-button']"));
+		login.click();
+		
+//		  username.clear();			
+//		  password.clear();
+				
+		
+		WebElement error = driver.findElement(By.xpath("//button[@class='error-button']"));
+		error.getText();
+	
+	}
+
+}
